56f9923a7dd283823895b496a763daa8828cd7c0
jython234 <jython234@users.noreply.github.com>
2018-06-13 22:44:41 -0500

    Fixed build command not being able to run CMake correctly

--- source/robomake/build.d
+++ source/robomake/build.d
@@ -22,9 +22,9 @@ version(Windows) {
 
 /// Runs the necessary commands to build a project in the current directory
 bool buildProject() @system {
-    immutable auto cmakeCommand = ["cmake", "\"-DCMAKE_TOOLCHAIN_FILE=arm-frc-toolchain.cmake\"",
-                            "-G", "\"" ~ cmakeGenerator ~ "\"", ".."];
-    immutable auto makeCommand = [makeExecutable, "-j" ~ to!string(threadsPerCPU())]; // -j[Number of threads our CPU has]
+    immutable auto cmakeCommand = "cmake -DCMAKE_TOOLCHAIN_FILE=arm-frc-toolchain.cmake" ~
+                                    " -G \"" ~ cmakeGenerator ~ "\" ..";
+    immutable auto makeCommand = makeExecutable ~ " -j" ~ to!string(threadsPerCPU()); // -j[Number of threads our CPU has]
 
     scope(exit) {
         chdir(".."); // Switch back to previous directory
@@ -39,7 +39,7 @@ bool buildProject() @system {
 
     writeInfo("Running CMake: " ~ to!string(cmakeCommand));
 
-    auto pid = spawnProcess(cmakeCommand);
+    auto pid = spawnShell(cmakeCommand);
     auto exitCode = wait(pid); // Wait for the command to complete
     if(exitCode != 0) {
         writeError("Build failure!");
@@ -48,7 +48,7 @@ bool buildProject() @system {
     }
 
     writeInfo("Running Make: " ~ to!string(makeCommand));
-    pid = spawnProcess(makeCommand);
+    pid = spawnShell(makeCommand);
     exitCode = wait(pid);
     if(exitCode != 0) {
         writeError("Build failure!");
